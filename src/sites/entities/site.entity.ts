71319429a072dd10ea6c43f37c316d09081b3af4
anthony-mini <anthony-mini@outlook.fr>
2023-11-14 17:00:32 +0100

    fix: add Entity()

--- src/sites/entities/site.entity.ts
+++ src/sites/entities/site.entity.ts
@@ -1,5 +1,8 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+// Entity présentes des données persistantes.
+
+@Entity()
 export class Site {
   @PrimaryGeneratedColumn({ name: 'id_site' })
   id: number;
