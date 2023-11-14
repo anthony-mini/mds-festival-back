b3bcc27b75d5386032ff50cab077fbd3d9a9e176
anthony-mini <anthony-mini@outlook.fr>
2023-11-14 12:34:54 +0100

    feat: add post event

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { EventsService } from './events.service';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventsService: EventsService) {}
+  constructor(
+    private readonly eventsService: EventsService,
+    @InjectDataSource() private bdd: DataSource,
+  ) {}
 
   @Get()
-  findAll(): string {
-    return this.eventsService.demo();
+  findAll() {
+    return this.bdd.query('SELECT * FROM evenement');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id_evenement: string) {
+    return this.bdd.query('SELECT * FROM evenement WHERE id_evenement = ?', [
+      +id_evenement,
+    ]);
+  }
+
+  @Post()
+  create(@Body() json) {
+    return this.bdd.query('INSERT INTO evenement SET ?', [json]);
+
+    /* Example to test with Postman in body raw JSON : 
+    *{
+      "id_scene": 1,
+      "id_edition": 1,
+      "titre": "Concert Ouverture",
+      "description": "Le concert d'ouverture du festival",
+      "debut": "2023-06-20 16:00:00",
+      "fin": "2023-06-20 19:00:00"
+    }
+    */
   }
 }
 
