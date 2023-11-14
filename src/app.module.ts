324241f982ce79d1ff1a9876ab205d84334b9d2e
anthony-mini <anthony-mini@outlook.fr>
2023-11-14 10:14:04 +0100

    step: 2)Install TypeOrm for DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 8889,
+      username: 'root',
+      password: 'root',
+      database: 'festival',
+    }),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
